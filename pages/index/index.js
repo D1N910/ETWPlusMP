65c5869688827270b0b5dffc74a19a33bd9229b1
D1n910 <wyd1n910@gmail.com>
2018-09-16 00:53:22 +0800

    主页面已经定好内容

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,14 @@
 // pages/index/index.js
 var app = getApp()
+var loadingInterval
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    dataList:[]
+    dataList:[],
+    isRefreshing: false
   },
 
   /**
@@ -14,7 +16,7 @@ Page({
    */
   onLoad: function (options) {
     wx.setBackgroundTextStyle({
-      textStyle: 'light', // 下拉背景字体、loading 图的样式为dark
+      textStyle: 'dark'
     })
     wx.startPullDownRefresh()    
   },
@@ -24,7 +26,12 @@ Page({
       this.setData({
         dataList: app.gobalData.audioList
       },()=>{
-        wx.stopPullDownRefresh()
+        setTimeout(()=>{
+          wx.stopPullDownRefresh()
+          this.setData({
+            isRefreshing: false
+          })
+        }, 1000)
       })
     }else{
       var LoadingResourcesSet = setTimeout(()=>{
@@ -62,9 +69,46 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
+    clearInterval(loadingInterval)
     this.LoadingResources()
+    // if (this.data.isRefreshing) {
+    //   return
+    // }
+    this.setData({
+      isRefreshing: true
+    },()=>{
+      // loading 动效
+      loadingInterval = setInterval(()=>{
+        this.loadingAniamtion()
+      },1200)
+    })
   },
 
+  /**
+   * loading 动效
+   */
+  loadingAniamtion() {
+    var loadingAniamtionData = wx.createAnimation({
+      duration: 600,
+      timingFunction: 'ease',
+    })
+
+    this.loadingAniamtionData = loadingAniamtionData
+
+    loadingAniamtionData.rotateX(360).step()
+
+    this.setData({
+      loading: loadingAniamtionData.export()
+    })
+    setTimeout(()=>{
+      loadingAniamtionData.rotateX(-360).step()
+
+      this.setData({
+        loading: loadingAniamtionData.export()
+      })
+
+    },600)
+  },
   /**
    * 页面上拉触底事件的处理函数
    */
