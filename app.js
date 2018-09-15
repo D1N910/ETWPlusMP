fa042a0c80048753ee96a9fda17d1c02d0cb2bd3
D1n910 <wyd1n910@gmail.com>
2018-09-15 10:13:28 +0800

    do制作首页；

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
+  onLaunch() {
+    var that = this
+    wx.cloud.init({
+      env: 'etwplus-test-485c18'
     })
-    // 获取用户信息
-    wx.getSetting({
+    const db = wx.cloud.database()
+    db.collection('audioList').where({
+    }).get({
       success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+        console.log(res.data)
+        this.gobalData.audioList = res.data
+        for (let i in this.gobalData.audioList){
+          wx.downloadFile({
+            url: this.gobalData.audioList[i].header, //仅为示例，并非真实的资源
+            success(res) {
+              if (res.statusCode === 200) {
+                that.gobalData.audioList[i].tempFilePath = res.tempFilePath
+                console.log(res.tempFilePath)
               }
             }
           })
+
         }
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
       }
     })
   },
-  globalData: {
-    userInfo: null
+  gobalData: {
   }
 })
---
+++ pages/startup/index.js
@@ -0,0 +1,80 @@
+var Audio = wx.createInnerAudioContext()
+
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    enterShowAnimationData: [],
+    seloganLAnimationData: [],
+    color: '#000000'
+  },
+
+  /**
+   * 跳转页面
+   */
+  enter() {
+    Audio.destroy()
+    wx.reLaunch({
+      url: '../index/index'
+    })
+  },
+  onLoad() {
+    Audio.src = 'https://audio.fireside.fm/podcasts/audio/8/8dd8a56f-9636-415a-8c00-f9ca6778e511/episodes/b/b57ebd4d-31e9-409a-8f1b-a900a23c238b/b57ebd4d-31e9-409a-8f1b-a900a23c238b.mp3'
+    Audio.play()
+    setTimeout(()=>{
+      Audio.stop()
+    },5000)
+  },
+  onReady() {
+    // 标语动效
+    var seloganAnimationData = wx.createAnimation({
+      duration: 1200,
+      timingFunction: 'ease',
+    })
+    
+    this.seloganAnimationData = seloganAnimationData
+
+    seloganAnimationData.opacity(1).step()
+
+    this.setData({
+      seloganAnimationData: seloganAnimationData.export()
+    })
+
+    // 进入按钮动效
+    var enterShowAnimationData = wx.createAnimation({
+      duration: 600,
+      timingFunction: 'ease',
+    })
+  
+    this.enterShowAnimationData = enterShowAnimationData
+
+    enterShowAnimationData.opacity(1).step()
+
+    setTimeout(()=>{
+
+      this.setData({
+        enterShowAnimationData: enterShowAnimationData.export()
+      })
+      setTimeout(function () {
+        enterShowAnimationData = wx.createAnimation({
+          duration: 1200,
+          timingFunction: 'ease',
+        })
+
+        this.enterShowAnimationData = enterShowAnimationData
+
+        enterShowAnimationData.rotate(-180).step()
+        this.setData({
+          enterShowAnimationData: enterShowAnimationData.export(),
+        })
+        setTimeout(()=>{
+          this.setData({
+            color: '#fc4236'                      
+          })
+        },1000)
+      }.bind(this), 800)
+    },1000)
+  }
+})
