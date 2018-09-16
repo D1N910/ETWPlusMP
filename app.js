0acb376907564e12cb15f5317f5b866877be2c83
D1n910 <wyd1n910@gmail.com>
2018-09-16 21:39:38 +0800

    开始制作播放器

--- app.js
+++ app.js
@@ -15,36 +15,36 @@ App({
       }
     })
     // 获取首页轮播图内容
-    wx.cloud.init({
-      env: 'etwplus-test-485c18'
-    })
-    const db = wx.cloud.database()
-    db.collection('audioList').where({
-    }).get({
-      success: res => {
-        console.log(res.data)
-        this.gobalData.audioList = res.data
-        for (let i in this.gobalData.audioList){
-          wx.downloadFile({
-            url: this.gobalData.audioList[i].header, //仅为示例，并非真实的资源
-            success(res) {
-              if (res.statusCode === 200) {
-                that.gobalData.audioList[i].tempFilePath = res.tempFilePath
-                console.log(res.tempFilePath)
-              }
-            }
-          })
+    // wx.cloud.init({
+    //   env: 'etwplus-test-485c18'
+    // })
+    // const db = wx.cloud.database()
+    // db.collection('audioList').where({
+    // }).get({
+    //   success: res => {
+    //     console.log(res.data)
+    //     this.gobalData.audioList = res.data
+    //     for (let i in this.gobalData.audioList){
+    //       wx.downloadFile({
+    //         url: this.gobalData.audioList[i].header, 
+    //         success(res) {
+    //           if (res.statusCode === 200) {
+    //             that.gobalData.audioList[i].tempFilePath = res.tempFilePath
+    //             console.log(res.tempFilePath)
+    //           }
+    //         }
+    //       })
 
-        }
-      },
-      fail: err => {
-        wx.showToast({
-          icon: 'none',
-          title: '查询记录失败'
-        })
-        console.error('[数据库] [查询记录] 失败：', err)
-      }
-    })
+    //     }
+    //   },
+    //   fail: err => {
+    //     wx.showToast({
+    //       icon: 'none',
+    //       title: '查询记录失败'
+    //     })
+    //     console.error('[数据库] [查询记录] 失败：', err)
+    //   }
+    // })
   },
   gobalData: {
   }
--- pages/audioPage/index.js
+++ pages/audioPage/index.js
@@ -5,14 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    coverImage: 'https://images.fireside.fm/podcasts/images/8/8dd8a56f-9636-415a-8c00-f9ca6778e511/episodes/e/efe15a9a-af08-4209-ba89-36ff79dfca60/header.jpg',
+    audioUrl:'https://aphid.fireside.fm/d/1437767933/8dd8a56f-9636-415a-8c00-f9ca6778e511/efe15a9a-af08-4209-ba89-36ff79dfca60.mp3',
+    logoVisiable: true
   },
 
+  /**
+   * 图片加载完成
+   */
+  haveLoad(){
+    this.setData({
+      logoVisiable:false      
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
   },
 
   /**
